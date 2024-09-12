a926854a8f357eb52217e9871d45d012af75cef7
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-12 09:40:19 +0100

    Feat: add of favicon

--- components/global/home/about/index.tsx
+++ components/global/home/about/index.tsx
@@ -1,8 +1,31 @@
 export default function HomeAbout() {
   return (
-    <section className="">
+    <section className="flex justify-between w-10/12 mx-auto h-screen py-20">
       <div></div>
-      <div></div>
+      <div className="w-1/2 mx-20">
+        <h1 className="font-bold text-blue_itm_aqua_marine mb-2">
+          QUI SOMMES-NOUS ?
+        </h1>
+        <h2 className="text-blue_itm_primary text-5xl font-bold mb-2">
+          ITM SARL
+        </h2>
+        <p>
+          ITM SARL, votre partenaire stratégique pour le développement de vos
+          ressources humaines. Spécialisés dans le recrutement, la formation et
+          le développement organisationnel, nous vous accompagnons dans toutes
+          les étapes de la gestion de vos talents. Grâce à notre expertise et à
+          notre réseau étendu, nous vous proposons des solutions sur mesure pour
+          répondre à vos besoins spécifiques.  Nous ne vous proposons pas de
+          formules toutes faites. Nous travaillons main dans la main avec vous
+          pour concevoir des solutions sur-mesure, adaptées à vos besoins
+          spécifiques et vos enjeux du moment. Chez ITM SARL, nous croyons que
+          le capital humain est la clé de la réussite. C'est pourquoi nous
+          accordons une importance particulière à la qualité de vie au travail
+          et au développement des compétences de vos collaborateurs. Notre
+          approche personnalisée vous permet de créer un environnement de
+          travail stimulant et motivant.
+        </p>
+      </div>
     </section>
   );
 }
--- components/global/home/banner/index.tsx
+++ components/global/home/banner/index.tsx
@@ -2,7 +2,7 @@ export default function HomePageBanner({ params }: { params: any }) {
   return (
     <>
       <section className="bg-blue_itm_aqua_marine/5 h-[90vh] w-full">
-        Content
+        Slider
       </section>
     </>
   );
