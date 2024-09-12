f09b53bc5c61d263b4e86681fe8dcb4e16afc21a
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-12 14:49:09 +0100

    Feature: add of banner carousel

--- components/global/home/banner/index.tsx
+++ components/global/home/banner/index.tsx
@@ -1,8 +1,10 @@
+import HomeBannerCarousel from './carousel';
+
 export default function HomePageBanner({ params }: { params: any }) {
   return (
     <>
-      <section className="bg-blue_itm_aqua_marine/10 h-[90vh] w-full">
-        Slider
+      <section className="bg-blue_itm_aqua_marine/10 h-[87vh] w-full">
+        <HomeBannerCarousel toShow={1} />
       </section>
     </>
   );
