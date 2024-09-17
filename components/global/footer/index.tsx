ba59623a659f2d92af7589f1fd845dd01de62be1
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-17 15:43:42 +0100

    Feature: add of footer

--- components/global/footer/index.tsx
+++ components/global/footer/index.tsx
@@ -2,20 +2,21 @@ import { getDictionary } from '@/get-dictionary';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '@/public/logos/logo_rdc_blanc.png';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 export default function Footer({ params }: { params: any }) {
   const lang = params.lang;
   const dictionary = getDictionary(lang);
   return (
-    <footer className="min-h-[70vh] bg-black_footer text-white">
-      <div className="flex w-11/12 mx-auto py-10">
-        <section className="w-1/3">
+    <footer className="bg-black_footer text-white">
+      <div className="flex w-11/12 mx-auto py-20 border-b border-b-black">
+        <section className="w-2/5">
           <div>
             <Link href={`/${lang}`} className="block w-fit h-fit md:pr-5 mr-1">
               <Image
                 src={logo.src}
                 className="md:h-8 h-4 w-fit"
-                height={90}
+                height={100}
                 width={100}
                 alt="ITM Africa logo"
                 style={{
@@ -35,12 +36,68 @@ export default function Footer({ params }: { params: any }) {
               type="text"
               placeholder="Votre email"
             />
-            <button className="py-2 px-4 border rounded">S'abonner</button>
+            <button className="py-2 px-4 border rounded hover:bg-blue_itm_aqua_marine hover:text-white hover:border-blue_itm_aqua_marine">
+              S'abonner
+            </button>
           </nav>
         </section>
-        <section className="w-2/3"></section>
+        <section className="flex justify-around ml-10 w-3/5">
+          <div>
+            <h4 className="text-2xl font-bold mb-4">A propos de nous</h4>
+            <ul className="mb-2">
+              <li className="mb-3">Services</li>
+              <li className="mb-3">Services</li>
+              <li className="mb-3">Services</li>
+              <li className="mb-3">Services</li>
+            </ul>
+            <Link href={lang} className="flex items-center font-bold text-lg">
+              Offres d'emplois
+              <span className="ml-1 text-xs">
+                <FaExternalLinkAlt />
+              </span>
+            </Link>
+          </div>
+          <div>
+            <h4 className="text-2xl font-bold mb-4">Nos logiciels</h4>
+            <ul className="mb-2">
+              <li className="mb-3">Talent pro</li>
+              <li className="mb-3">Kazi pro</li>
+            </ul>
+          </div>
+          <div>
+            <h4 className="text-2xl font-bold mb-4">Suivez-nous</h4>
+            <ul className="mb-2">
+              <li className="mb-3">Talent pro</li>
+              <li className="mb-3">Kazi pro</li>
+            </ul>
+          </div>
+        </section>
       </div>
-      <section></section>
+      <section className="flex justify-between w-11/12 mx-auto py-4">
+        <div className="w-fit font-bold">
+          {`© ${new Date().getFullYear()} ITM Africa. All rights reserved.`}
+        </div>
+        <div className="cursor-pointer md:mt-0 mt-2 w-full md:w-fit flex  flex-wrap items-center justify-between">
+          <Link
+            href={lang}
+            className="w-fit md:ml-4 hover:font-bold md:mb-0 mb-2 "
+          >
+            Paramètre des cookies
+          </Link>
+          <Link
+            href={lang}
+            className="w-fit md:ml-4 hover:font-bold md:mb-0 mb-2 "
+          >
+            Politique de confidentialité
+          </Link>
+          <Link
+            href={lang}
+            className="w-fit md:ml-4 hover:font-bold md:mb-0 mb-2 "
+          >
+            Condition d'utilisation
+          </Link>
+        </div>
+      </section>
     </footer>
   );
 }
