1b671a8bd0a008314cd301688798b362bef0804f
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-19 17:34:34 +0100

    Refactor: responsive of the testimonies section

--- components/home/testimonies/index.tsx
+++ components/home/testimonies/index.tsx
@@ -6,9 +6,9 @@ export default function HomeTestimonies({ params }: { params: any }) {
   const dictionary = getDictionary(lang);
   const data = dictionary.cd.pages.home.testimonies;
   return (
-    <section className="min-h-screen py-20">
+    <section className="min-h-screen md:py-20 py-8">
       <div>
-        <h2 className="w-2/6 mx-auto text-5xl font-bold text-blue_itm_primary text-center">
+        <h2 className="lg:w-2/6 md:w-1/2 w-full mx-auto md:text-5xl text-4xl font-bold text-blue_itm_primary text-center">
           {data.title}
         </h2>
         <div className="w-10/12 mx-auto">
