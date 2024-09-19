fb7635752a2c08789bd1c84bd4e52ce5dff95963
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-19 17:48:24 +0100

    Refactor: responsive of the call to action home

--- components/home/callToAction/index.tsx
+++ components/home/callToAction/index.tsx
@@ -1,9 +1,9 @@
 export default function HomeCallToAction({ params }: { params: any }) {
   return (
-    <section className="py-16 flex justify-center rounded-md overflow-hidden">
-      <div className="w-9/12 h-[350px] flex flex-wrap items-center justify-center bg-cover rounded-3xl overflow-hidden bg-no-repeat bg-[url('../public/bg.png')] bg-black/40">
+    <section className="md:py-16 py-8 flex justify-center rounded-md overflow-hidden">
+      <div className="md:w-9/12 w-11/12 md:h-[350px] h-[300px] flex flex-wrap items-center justify-center bg-cover rounded-3xl overflow-hidden bg-no-repeat bg-[url('../public/bg.png')] bg-black/40">
         <div className="w-full text-white">
-          <h1 className="text-center text-2xl md:text-5xl px-3 md:px-0  font-bold">
+          <h1 className="text-center text-3xl md:text-5xl px-3 md:px-0 font-bold">
             Boostez votre
             <span className="text-blue_itm_aqua_marine"> carrière </span>
             en un click
