ad98175f68200762c920754f4eee663da26ca8e3
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-19 16:31:00 +0100

    Refactor: responsive on the services section

--- components/home/services/card/index.tsx
+++ components/home/services/card/index.tsx
@@ -14,14 +14,14 @@ export default function HomeServiceCard({
   btnMore: any;
 }) {
   return (
-    <article className="shadow-md hover:shadow-xl rounded-lg px-10 py-10 w-[31%] mb-8">
+    <article className="shadow-md hover:shadow-xl rounded-lg md:p-10 p-8 lg:w-[31%] sm:w-[48%] w-full  mb-8">
       <div className="flex justify-center">
         <Image src={icon} alt="Icon" width={140} height={150} />
       </div>
       <h1 className="mb-5 text-center text-3xl text-blue_itm_good font-bold">
         {name}
       </h1>
-      <p className="mb-10 text-center font-medium">{paragraph}</p>
+      <p className="md:mb-10 mb-5 text-center font-medium">{paragraph}</p>
       <div className="flex justify-center">
         <ButtonOulined {...btnMore} />
       </div>
--- components/home/services/index.tsx
+++ components/home/services/index.tsx
@@ -9,8 +9,8 @@ export default function HomeServicesSection({ params }: { params: any }) {
   const data = dictionary.cd.pages.home.services;
 
   return (
-    <section className="min-h-screen md:w-9/12 w-11/12 mx-auto">
-      <div className="mb-20">
+    <section className="min-h-screen xl:w-9/12 w-11/12 mx-auto">
+      <div className="md:mb-20 mb-10">
         <SectionTitle text={data.title} center={true} />
         <h2 className="text-blue_itm_good md:text-5xl text-3xl font-bold text-center md:w-3/4 w-full mt-4 mb-8 mx-auto">
           {data.subtitle}
