4feee27c210c72a04f7932ec2b953d69c7fe1405
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-19 17:00:30 +0100

    Refactor: responsive of the metrics section

--- components/home/metrics/card/index.tsx
+++ components/home/metrics/card/index.tsx
@@ -10,7 +10,7 @@ export default function HomeMetricsCard({
   bg: boolean;
 }) {
   return bg ? (
-    <div className="group rounded-lg w-[23%] bg-blue_itm_primary hover:bg-transparent hover:text-black text-white py-4 px-5">
+    <div className="group rounded-lg lg:w-[23%] md:w-[48%] w-full bg-blue_itm_primary mb-4 lg:mb-0 hover:bg-transparent hover:text-black text-white py-4 px-5">
       <h2 className="text-3xl font-bold mb-2 group-hover:text-blue_itm_aqua_marine">
         {number}
       </h2>
@@ -18,7 +18,7 @@ export default function HomeMetricsCard({
       <p className="text-sm">{description}</p>
     </div>
   ) : (
-    <div className="rounded-lg w-[23%] py-4 px-5 group hover:bg-blue_itm_aqua_marine hover:text-white duration-50">
+    <div className="rounded-lg lg:w-[23%] md:w-[48%] w-full py-4 px-5 group hover:bg-blue_itm_aqua_marine mb-4 lg:mb-0 hover:text-white duration-50">
       <h2 className="text-3xl font-bold mb-2 text-blue_itm_aqua_marine group-hover:text-white duration-0">
         {number}
       </h2>
--- components/home/metrics/index.tsx
+++ components/home/metrics/index.tsx
@@ -6,15 +6,17 @@ export default function HomeMetrics({ params }: { params: any }) {
   const dictionary = getDictionary(lang);
   const data = dictionary.cd.pages.home.metrics;
   return (
-    <section className="flex items-center w-10/12 mx-auto min-h-screen">
+    <section className="flex items-center md:w-10/12 w-11/12 mx-auto min-h-screen">
       <div className="m-auto">
-        <div className="flex justify-between mb-16">
-          <h3 className="text-4xl font-bold w-1/4 text-blue_itm_primary">
+        <div className="md:flex justify-between md:mb-16 mb-10">
+          <h3 className="text-4xl font-bold md:w-1/4 w-4/5 mx-auto md:mx-0 text-blue_itm_primary">
             {data.title}
           </h3>
-          <p className="w-2/5 text-blue_itm_primary">{data.paragraph}</p>
+          <p className="md:w-2/5 w-full text-blue_itm_primary">
+            {data.paragraph}
+          </p>
         </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between flex-wrap">
           {data.metricsItems.map((item: any, index: number) => (
             <HomeMetricsCard {...item} key={index} />
           ))}
