e6dab0ca5b94731165e6f0ded77883c630042772
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-20 17:20:58 +0100

    Refactor: responsive of header (part)

--- components/global/header/index.tsx
+++ components/global/header/index.tsx
@@ -7,7 +7,7 @@ import logo from '@/public/logos/logo_rdc.png';
 import NavLinkCard from '../navLinkCard';
 import CardLang from '../cardLang';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineMenu, MdOutlineMenuOpen } from 'react-icons/md';
 
 type linkHeader = {
@@ -22,23 +22,37 @@ export default function HomeHeader({ params }: propsPage) {
   const dictionary: dictionary = getDictionary(lang);
   const data: any = dictionary?.global?.header.cd;
 
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const handleNavigation = () => {
+    setOpenNavigation((state: any) => !state);
+  };
+
   return (
     <header className="sticky z-50 top-0 w-full h-fit py-3 md:py-4 flex justify-center bg-white shadow-headerShadow">
       <div className="w-11/12 h-fit flex items-center justify-between ">
         <div className="w-fit flex items-center">
-          <Link href={`/${lang}`} className="block w-fit h-fit md:pr-5 mr-1">
-            <Image
-              src={logo.src}
-              className="md:h-8 h-4 w-fit"
-              height={60}
-              width={100}
-              alt="ITM Africa logo"
-              style={{
-                maxWidth: '100%',
-                height: 'auto',
-              }}
-            />
-          </Link>
+          <div className="flex items-center">
+            <div
+              onClick={handleNavigation}
+              className="w-fit lg:hidden mr-1 text-2xl p-1 cursor-pointer pl-0 rounded-md hover:pl-1 hover:bg-blue_itm_primary/10"
+            >
+              <MdOutlineMenu />
+            </div>
+            <Link href={`/${lang}`} className="block w-fit h-fit md:pr-5 mr-1">
+              <Image
+                src={logo.src}
+                className="md:h-8 h-4 w-fit"
+                height={60}
+                width={100}
+                alt="ITM Africa logo"
+                style={{
+                  maxWidth: '100%',
+                  height: 'auto',
+                }}
+              />
+            </Link>
+          </div>
         </div>
         <nav className="w-fit lg:flex hidden items-center justify-evenly">
           {data?.links.map((item: linkHeader, index: number) => (
@@ -46,6 +60,46 @@ export default function HomeHeader({ params }: propsPage) {
           ))}
         </nav>
         <CardLang params={params} links={data?.links} />
+        {
+          <div className="w-full lg:hidden flex z-50 bg-black/80 h-full fixed top-0 right-0 bottom-0 left-0">
+            <div className="w-9/12 p-5 h-full bg-white rounded-e-xl flex flex-col justify-between">
+              <div className="w-full">
+                <div className="w-full flex items-center justify-between px-4 mb-3">
+                  <div
+                    onClick={closeSideBarRoot}
+                    className="w-fit md:hidden mr-1 text-2xl p-1 cursor-pointer pl-0 rounded-md hover:pl-1 hover:bg-blue_itm_primary/10"
+                  >
+                    <MdOutlineMenuOpen />
+                  </div>
+                  <Link
+                    href={`/${lang}`}
+                    onClick={closeSideBarRoot}
+                    className="block w-fit h-fit md:pr-5 mr-1"
+                  >
+                    <Image
+                      src={logo.src}
+                      className="md:h-8 h-4 w-fit"
+                      height={50}
+                      width={194}
+                      alt="ITM TALENT PRO"
+                      style={{
+                        maxWidth: '100%',
+                        height: 'auto',
+                      }}
+                    />
+                  </Link>
+                </div>
+                <nav className="w-full bg-white items-center justify-evenly">
+                  {data?.links
+                    .filter((item) => item.type === 'link')
+                    .map((item: linkHeader, index: number) => (
+                      <CardLink {...item} key={index} />
+                    ))}
+                </nav>
+              </div>
+            </div>
+          </div>
+        }
       </div>
     </header>
   );
