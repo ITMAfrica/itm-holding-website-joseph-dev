63db500f551a0086210012e384b94617a2f56e64
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-23 15:01:32 +0100

    Refactor: fix of the build

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,20 +1,5 @@
-import { cookies } from 'next/headers';
-import { permanentRedirect } from 'next/navigation';
 import HomePage from '@/components/pages/home';
 
-export default async function IndexPage({ params }: { params: any }) {
-  const cookieStore = cookies();
-  const isCountryDefined = cookieStore.has('country');
-
-  if (isCountryDefined) {
-    const countryName = cookieStore.get('country');
-    permanentRedirect(`/${countryName}`);
-  }
-  console.log('Country exist', isCountryDefined);
-
-  return (
-    <>
-      <HomePage params={params} />
-    </>
-  );
+export default function IndexPage({ params }: { params: any }) {
+  return <HomePage params={params} />;
 }
