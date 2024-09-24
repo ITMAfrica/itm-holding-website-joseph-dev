94dd1fb6ec727eaaadb54c7217fb588a8fb34317
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-24 14:35:42 +0100

    Refactor: add of testimonies item

--- components/home/testimonies/carouselItem/index.tsx
+++ components/home/testimonies/carouselItem/index.tsx
@@ -1,7 +1,33 @@
+import { BiSolidQuoteAltLeft } from 'react-icons/bi';
+import { BiSolidQuoteAltRight } from 'react-icons/bi';
+
 export default function TestimonyItem({ item }: any) {
   return (
     <article className="relative mx-2 w-[350px] h-[350px] border shadow-lg">
-      <div className="">{item}</div>
+      <div className="p-6">
+        {item}
+        <div>Image</div>
+
+        <div>
+          <main>
+            <span className="text-gray-300 text-3xl">
+              <BiSolidQuoteAltLeft />
+            </span>
+            <p className="text-sm py-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+              debitis iure id deleniti molestias reprehenderit voluptas cumque
+              fugiat.
+            </p>
+            <span className="flex w-full justify-end text-gray-300 text-3xl">
+              <BiSolidQuoteAltRight />
+            </span>
+          </main>
+          <h3 className="text-center font-bold text-blue_itm_aqua_marine">
+            Amos BAFUNA
+          </h3>
+          <h6 className="text-center">Web developer</h6>
+        </div>
+      </div>
     </article>
   );
 }
