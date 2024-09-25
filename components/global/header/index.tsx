dd384e9a44ea93d9ddabfde098d2b252be4d8cc4
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-09-25 11:59:31 +0100

    Refactor: fix of the header on the responsive

--- components/global/header/index.tsx
+++ components/global/header/index.tsx
@@ -64,36 +64,39 @@ export default function HomeHeader({ params }: propsPage) {
           <div className="w-full lg:hidden flex z-50 bg-black/80 h-full fixed top-0 right-0 bottom-0 left-0">
             <div className="w-9/12 p-5 h-full bg-white rounded-e-xl flex flex-col justify-between">
               <div className="w-full">
-                <div className="w-full flex items-center justify-between px-4 mb-3">
+                <div className="w-full flex items-center justify-between mb-6">
                   <div
                     onClick={handleNavigation}
                     className="w-fit lg:hidden mr-1 text-2xl p-1 cursor-pointer pl-0 rounded-md hover:pl-1 hover:bg-blue_itm_primary/10"
                   >
                     <MdOutlineMenuOpen />
                   </div>
-                  <Link
-                    href={`/${lang}`}
-                    onClick={handleNavigation}
-                    className="block w-fit h-fit md:pr-5 mr-1"
-                  >
-                    <Image
-                      src={logo.src}
-                      className="md:h-8 h-4 w-fit"
-                      height={50}
-                      width={194}
-                      alt="ITM Africa"
-                      style={{
-                        maxWidth: '100%',
-                        height: 'auto',
-                      }}
-                    />
-                  </Link>
+                  <div>
+                    <Link
+                      href={`/${lang}`}
+                      onClick={handleNavigation}
+                      className="block w-fit h-fit md:pr-5 mr-1"
+                    >
+                      <Image
+                        src={logo.src}
+                        className="md:h-8 h-4 w-fit"
+                        height={50}
+                        width={100}
+                        alt="ITM Africa"
+                        style={{
+                          maxWidth: '100%',
+                          height: 'auto',
+                        }}
+                      />
+                    </Link>
+                  </div>
                 </div>
                 <nav className="w-full bg-white items-center justify-evenly">
                   {data?.links.map((item: linkHeader, index: number) => (
                     <Link
                       href={item.href}
                       key={index}
+                      onClick={handleNavigation}
                       className="block mb-2 hover:text-blue_itm_good"
                     >
                       {item.name}
