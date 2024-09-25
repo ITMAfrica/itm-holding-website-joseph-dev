af7cb728a7e2650182b13f8a68aba35624ca9edf
jeereq <mingandajeereq@gmail.com>
2024-09-25 19:26:07 +0100

    feat: save change in cards

--- components/global/cards/lang/index.tsx
+++ components/global/cards/lang/index.tsx
@@ -78,35 +78,6 @@ export default function CardLang({
 
   return (
     <>
-      {/* <div className="w-fit flex items-center font-bold overflow-hidden bg-gray_itm_bg/40 border-2 border-gray_itm_bg/40 ">
-        {currentPage?.map((item, i: number) => (
-          <Link
-            key={i}
-            href={item.href}
-            className={`w-fit flex items-center align-middle p-1 ${
-              !splitHref(item?.href) ? 'bg-gray_itm_bg/40' : 'bg-white'
-            }`}
-          >
-            <Image
-              src={images[i]}
-              height={12 * 0.6}
-              width={16 * 0.6}
-              className="h-fit w-fit md:block hidden"
-              alt="drapeau"
-            />
-            <Image
-              src={images[i]}
-              height={12 * 0.2}
-              width={16 * 0.2}
-              className="h-fit w-fit md:hidden block"
-              alt="drapeau"
-            />
-            <span className="text-blue_itm_title_blue md:text-sm text-xs pt-0.5 align-middle uppercase mr-1">
-              {item.name}
-            </span>
-          </Link>
-        ))}
-      </div> */}
       <div className="flex justify-center items-center">
         <Image
           src={fr}
--- components/global/modal/countryChoice/index.tsx
+++ components/global/modal/countryChoice/index.tsx
@@ -7,11 +7,11 @@ import CardCountryChoice from "@/components/global/cards/countryChoice";
 import { useParams, usePathname } from "next/navigation";
 import { getDictionary } from "@/get-dictionary";
 
-export default function ModalCountryChoice() {
+export default function ModalCountryChoice({ init = false }: any) {
     const params: any = useParams()
     const lang: string = params.lang
     const pathname = usePathname()
-    const [modal, setModal] = useState(true)
+    const [modal, setModal] = useState(init)
     const dictionary = getDictionary(lang)
     const data: any = dictionary.global.modal[CODE]
 
@@ -30,6 +30,13 @@ export default function ModalCountryChoice() {
             closeModal()
         }
     }, [pathname])
+
+    useEffect(function () {
+        if (init) {
+            openModal()
+        }
+    }, [init])
+
 
     if (modal)
         return <section className="w-full z-50 fixed bg-black/30 backdrop-blur-sm flex items-center flex-wrap justify-center top-0 left-0 right-0 bottom-0">
