46fe109ef6f2ad79042686f7256b7c43030e1605
jeereq <mingandajeereq@gmail.com>
2024-09-26 09:24:09 +0100

    feat: save change

--- components/global/callToAction/index.tsx
+++ components/global/callToAction/index.tsx
@@ -10,20 +10,20 @@ export default function CallToAction({ params }: { params: any }) {
       <div className="md:w-9/12 w-11/12 md:h-[350px] h-[300px] flex flex-wrap items-center justify-center bg-cover rounded-3xl overflow-hidden bg-no-repeat bg-[url('../public/bg.png')] bg-black/40">
         <div className="w-full text-white">
           <h1 className="text-center text-3xl md:text-5xl px-3 md:px-0 font-bold">
-            {data.paragraph1}
-            <span className="text-blue_itm_aqua_marine">{data.career}</span>
-            {data.click}
+            {data?.paragraph1}
+            <span className="text-blue_itm_aqua_marine">{data?.career}</span>
+            {data?.click}
           </h1>
           <p className="text-center pt-2 pb-5 text-xl w-11/12 m-auto md:w-full">
-            {data.subtitle}
+            {data?.subtitle}
           </p>
           <div className="flex justify-center">
             <Link
-              href={data.btn.link}
+              href={data?.btn.link}
               className="px-6 py-2 bg-blue_itm_aqua_marine rounded-full"
-              title={data.btn.title}
+              title={data?.btn.title}
             >
-              {data.btn.text}
+              {data?.btn.text}
             </Link>
           </div>
         </div>
