6338bca450b94225d6ebc040211adb63b2202c71
jeereq <mingandajeereq@gmail.com>
2024-09-26 10:55:42 +0100

    feat: save change in services page

--- components/pages/services/index.tsx
+++ components/pages/services/index.tsx
@@ -1,9 +1,11 @@
 import CallToAction from "@/components/global/callToAction"
+import ServicesPageAbout from "@/components/services/about"
 import ServicesPageBanner from "@/components/services/banner"
 
 export default function ServicesPage({ params }: any) {
     return <main className="w-full">
         <ServicesPageBanner params={params} />
+        <ServicesPageAbout params={params} />
         <CallToAction params={params} />
     </main>
 }
