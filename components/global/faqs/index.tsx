19e1111c7484c99ab9b7f29e014d033901e0836f
jeereq <mingandajeereq@gmail.com>
2024-09-26 15:02:33 +0100

    feat: save chaneg in faqs section and card

--- components/global/faqs/index.tsx
+++ components/global/faqs/index.tsx
@@ -1,3 +1,24 @@
-export default function Faqs() {
-    return <section className="w-full py-20"></section>
+import SectionTitle from "@/components/global/section_title";
+import CardFaq from "@/components/global/cards/faq";
+
+export default function Faqs({ faqs = Array.from({ length: 10 }) }) {
+    return <section className="w-full py-20">
+        <div className="w-11/12 lg:w-10/12 mx-auto h-[500px] lg:flex">
+            <div className="w-full lg:w-1/3">
+                <SectionTitle text="QUESTIONS" />
+                <h1 className="font-bold  text-4xl  text-blue_itm_primary">
+                    Foire aux questions
+                </h1>
+                <p className="pt-5">
+                    N'hésitez pas à nous poser toutes vos questions <br />
+                    Nous sommes là pour vous
+                </p>
+            </div>
+            <div className="w-full lg:w-2/3 h-full overflow-y-scroll">
+                {faqs.map(function (item: any, index: number) {
+                    return <CardFaq {...item} key={index} />
+                })}
+            </div>
+        </div>
+    </section>
 }
