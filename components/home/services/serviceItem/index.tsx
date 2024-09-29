254c2f71e4bec4ac2228dcc01285d5174cac6482
Amos Bafuna <amosbafuna24@gmail.com>
2024-09-29 23:27:41 +0100

    Refactor: add of service card

--- components/home/services/serviceItem/index.tsx
+++ components/home/services/serviceItem/index.tsx
@@ -1,5 +1,6 @@
 import ButtonOulined from '@/components/global/buttons/btn_outlined';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function HomeServiceCard({
   name,
@@ -15,13 +16,16 @@ export default function HomeServiceCard({
   activeSlide: any;
 }) {
   return (
-    <article className="border h-[400px] mx-3 bg-black/70 md:p-10 p-8 lg:w-[31%] group sm:w-[48%] w-full mb-8">
-      <h1 className="mb-5 text-center text-3xl text-blue_itm_good font-bold">
-        Gestion du personnel
-      </h1>
-
-      <div className="flex justify-center">
-        <ButtonOulined {...btnMore} />
+    <article className="border h-[400px] mx-3 bg-black/70 lg:w-[31%] group sm:w-[48%] w-full mb-8">
+      <div className="w-[18rem]">
+        <h1 className="mb-5 text-xl text-center text-white font-bold">
+          Gestion du personnel
+        </h1>
+        <div className="flex justify-center">
+          <Link className="border-white py-2 px-3 rounded-foull" href={'/'}>
+            Lire plus
+          </Link>
+        </div>
       </div>
     </article>
   );
