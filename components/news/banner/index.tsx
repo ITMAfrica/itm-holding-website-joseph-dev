fb7d3d12316ceeb024802ef6b6b946114afd5028
jeereq <mingandajeereq@gmail.com>
2024-09-30 09:41:26 +0100

    feat: save in card nbews

--- components/news/banner/index.tsx
+++ components/news/banner/index.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+import bg from "@/public/assets/bg/bg.news.png"
 import CardNews from "../card";
 
 export default function NewsPageBanner() {
     return <section className="w-full h-fit lg:h-[80vh] py-10">
         <div className="w-11/12 lg:w-11/12 mx-auto h-full flex lg:flex-nowrap flex-wrap">
-            <div className="w-full lg:w-9/12 h-[80vh] lg:h-full bg-red-500 border lg:mr-5 mr-0 relative">
-
+            <div className="w-full lg:w-9/12 h-[80vh] lg:h-full border lg:mr-5 mr-0 relative">
+                <Image src={bg} alt="" fill />
             </div>
             <div className="h-full w-3/12  lg:pt-0 pt-5 overflow-y-scroll scrollbar-thin">
                 <CardNews full />
--- components/news/card/index.tsx
+++ components/news/card/index.tsx
@@ -1,10 +1,25 @@
+import bg from "@/public/assets/bg/bg.news.png"
+import Image from "next/image"
 export default function CardNews({ full = true }: any) {
     if (full) {
-        return <article className="w-full h-[100px] flex items-center justify-start mb-3 bg-yellow-500">
-            <div className="w-[100px] h-[100px] bg-red-500"></div>
+        return <article className="w-full h-[100px] flex items-center justify-start mb-3">
+            <div className="w-[100px] h-[100px] relative">
+                <Image src={bg} alt="" fill />
+            </div>
+            <div className="w-full">
+                <h1 className="font-bold">
+
+                </h1>
+                <p className="pl-3 text-xs">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eligendi vero laboriosam sapiente illo accusantium culpa
+                </p>
+            </div>
         </article>
     }
-    return <article className="w-[20vw] h-[100px] mb-5 bg-yellow-500">
-        <div className="w-[100px] h-[100px] bg-red-500"></div>
+    return <article className="w-[20vw] h-[100px] mb-5">
+        <div className="w-[100px] h-[100px] relative">
+            <Image src={bg} alt="" fill />
+        </div>
     </article>
 }
