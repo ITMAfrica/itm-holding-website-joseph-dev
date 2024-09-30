c5d5c5526e8b73880695132ddd8e66361e192052
jeereq <mingandajeereq@gmail.com>
2024-09-30 11:11:28 +0100

    feat: save change in carousel seconfary section

--- components/news/secondaryNews/index.tsx
+++ components/news/secondaryNews/index.tsx
@@ -1,21 +1,76 @@
+"use client"
 import Image from "next/image";
+import Carousel from 'react-simply-carousel';
 import bg from "@/public/assets/bg/bg.news.png"
 import CardNews from "../card";
+import { useRef, useState } from "react";
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
 
-export default function NewsPageSecondary() {
+export default function NewsPageSecondary({ toShow = 1, data = Array.from({ length: 10 }) }) {
+    const [activeSlide, setActiveSlide] = useState(0);
+    const refRight = useRef(null)
     return <section className="w-full bg-white lg:py-20 py-10">
         <div className="w-11/12 lg:w-10/12 mx-auto h-full ">
-
             <main className="w-full mr-0">
-                <header className="h-fit w-full">
-                    <h1 className="font-bold text-blue_itm_good w-full pb-3 text-3xl">
-                        Autres nouvelles
-                    </h1>
-                    <div className="w-1/12 border-blue_itm_good border-b-2 mb-5"></div>
+                <header className="h-fit w-full flex justify-between items-center  mb-5">
+                    <div className="w-fit items-center flex flex-wrap ">
+                        <h1 className="font-bold text-blue_itm_good w-full w-full pb-3 text-3xl">
+                            Autres nouvelles
+                        </h1>
+                        <div className="w-9/12 border-blue_itm_good border-b-2"></div>
+                    </div>
+                    <div className="w-fit flex items-center cursor-pointer">
+                        <div onClick={function () {
+                            if (activeSlide != data.length - 1)
+                                setActiveSlide(activeSlide + 1);
+                        }} className=" h-10 w-10 hover:bg-blue_itm_good bg-blue_itm_good/50 text-white flex items-center justify-center rounded-full">
+                            <MdOutlineKeyboardArrowLeft />
+                        </div>
+                        <div onClick={function () {
+                            if (activeSlide != 0) {
+                                setActiveSlide(activeSlide - 1);
+                            }
+                        }} className="ml-2 h-10 w-10 hover:bg-blue_itm_good bg-blue_itm_good/50  text-white flex items-center justify-center rounded-full">
+                            <MdOutlineKeyboardArrowRight />
+                        </div>
+
+                    </div>
                 </header>
-                <div className="w-full h-[80vh] pt-5 relative">
-                <Image src={bg} alt="" fill objectFit="cover" />
-                </div>
+                <Carousel infinite={true}
+                    containerProps={{
+                        style: {
+                            width: '100%',
+                            height: '100%',
+                            justifyContent: 'space-between',
+                            borderRadius: 20,
+                        },
+                    }}
+                    itemsToShow={toShow}
+                    onRequestChange={setActiveSlide}
+                    easing="linear"
+
+                    responsiveProps={[
+                        {
+                            itemsToShow: toShow,
+                            itemsToScroll: 1,
+                            minWidth: 768,
+                        },
+                    ]}
+                    preventScrollOnSwipe
+                    activeSlideIndex={activeSlide}
+                    speed={1000}
+                >
+                    {data.map((item: any, index: number) => (
+
+                        <div className="w-[90vw] h-[80vh] pt-5 relative">
+                            <div className="absolute z-10 bg-green-500 top-3 left-3 text-base  px-4 py-2 text-white ">
+                                review
+                            </div>
+                            <Image src={bg} alt="" fill objectFit="cover" />
+                        </div>
+                    ))}
+                </Carousel>
+
             </main>
             <aside className="h-full w-full pt-5 overflow-y-scroll scrollbar-thin">
                 <div className="W-full flex flex-wrap justify-between">
