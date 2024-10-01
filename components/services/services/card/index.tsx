24eb6591ec839381778a61d4fa11a56a470682ec
jeereq <mingandajeereq@gmail.com>
2024-10-01 12:27:03 +0100

    feat: save change inservices card

--- components/services/services/card/index.tsx
+++ components/services/services/card/index.tsx
@@ -1,63 +1,22 @@
-import SectionTitle from "@/components/global/section_title"
-import { servicesHome } from "@/lib/data"
 import Image from "next/image"
 import { useParams } from "next/navigation"
 
-export default function CardServices({ name, index, image, activeSlide, setActiveSlide, description, left = true }: any) {
-    const params: any = useParams()
-    const lang: string = params.lang
+export default function CardServices({ name, index, image, activeSlide, setActiveSlide, description }: any) {
+
 
     return <article onClick={function () {
         setActiveSlide(index)
-    }} className={`h-full w-[20vw] shadow rounded-2xl overflow-hidden ml-5 ${index == activeSlide ? '' : ''} relative`}>
-        <Image src={servicesHome[lang][index].image} alt="" fill objectFit="cover" />
-        <div className="absolute top-0 text-white bg-gradient-to-b from-transparent to-black left-0 right-0 bottom-0 p-5 flex items-end justifty-center">
+    }} className={`h-[50vh] w-[20vw] shadow-2xl shadow-white overflow-hidden mr-5 ${index == activeSlide ? '' : ''} relative`}>
+        <Image src={image} alt="" fill objectFit="cover" />
+        <div className="absolute top-0 text-white bg-gradient-to-b from-transparent to-black left-0 right-0 bottom-0 p-10 flex items-end justifty-center">
             <div className="w-full">
                 <h1 className="font-bold text-xl">
-                    {servicesHome[lang][index]?.name}
+                    {name}
                 </h1>
                 
                 <p className="pt-5 text-xs">
-                    {servicesHome[lang][index]?.description.slice(0, 50)}...
+                    {description.slice(0, 50)}...
                 </p>
-            </div>
-        </div>
-    </article>
-    if (left) {
-        return <article className="w-full min-h-[400px] group  flex justify-center ease-linear">
-            <div className="w-full flex">
-                <div className="lg:w-1/2 w-full h-full group-hover:bg-gray_itm_bg/40 py-20">
-                    <div className="lg:w-8/12 relative z-10 w-11/12 mx-auto bg-white lg:group-hover:translate-x-2/3 lg:p-10 p-0 lg:pl-0 lg:group-hover:p-10 group-hover:p-5 ">
-                        <SectionTitle text={"Nos Services"} />
-                        <h1 className="font-bold  text-4xl  text-blue_itm_primary">
-                            {name}
-                        </h1>
-                        <p>
-                            {description}
-                        </p>
-                    </div>
-                </div>
-                <div className="w-1/2  lg:block hidden h-full bg-gray_itm_bg/40 relative">
-                    <Image src={image} fill alt={name} />
-                </div>
-            </div>
-        </article>
-    }
-    return <article className="w-full min-h-[400px] group flex justify-center ease-linear">
-        <div className="w-full flex">
-            <div className="w-1/2 lg:block hidden h-full bg-gray_itm_bg/40 relative">
-                <Image src={image} fill alt={name} />
-            </div>
-            <div className="lg:w-1/2 w-full h-full group-hover:bg-gray_itm_bg/40 py-20">
-                <div className="lg:w-8/12 relative w-11/12 mx-auto bg-white lg:p-10 p-0 lg:pr-0 lg:group-hover:-translate-x-2/3 lg:group-hover:p-10 group-hover:p-5 ">
-                    <SectionTitle text={"Nos Services"} />
-                    <h1 className="font-bold  text-4xl  text-blue_itm_primary">
-                        {name}
-                    </h1>
-                    <p>
-                        {description}
-                    </p>
-                </div>
             </div>
         </div>
     </article>
--- components/services/services/index.tsx
+++ components/services/services/index.tsx
@@ -10,42 +10,31 @@ export default function ServicesPageServices({ params }: { params: any }) {
     const lang = params.lang
     const [toShow] = useState(3)
     const [activeSlide, setActiveSlide] = useState(0);
-    return <section className="w-full min-h-screen  flex items-center bg-white relative">
+    return <section className="w-full min-h-[80vh] lg:py-20 py-10 flex items-center bg-white relative">
         <Image alt="" src={servicesHome[lang][activeSlide]?.image} fill objectFit="cover" />
         <div className="w-full bg-gradient-to-l from-transparent to-black absolute z-10 top-0 left-0 right-0 bottom-0 flex items-center justify-center">
             <article className="w-full lg:w-1/2 h-full flex items-center justify-center text-white">
                 <div className="w-10/12 h-10/12 lg:w-8/12 h-8/12">
-                    <h1 className="font-bold text-6xl">
+                    <h1 className="font-bold text-5xl">
                         {servicesHome[lang][activeSlide]?.name}
                     </h1>
-                    <p className="pt-3">
-                        Lorem ipsum dolor sit amet consectetur. Orci amet feugiat volutpat amet eget tincidunt non cursus orci. Consectetur nec neque imperdiet a sed ornare. Non est purus convallis morbi turpis mauris. Sit sed risus amet consectetur ac senectus faucibus. Lectus cursus purus aliquet sem velit. Platea bibendum vitae luctus purus. Commodo gravida fermentum felis turpis
-                    </p>
+                   
                     <p className="pt-3">
                         {servicesHome[lang][activeSlide]?.description}
                     </p>
                     <div className="w-full pt-5 flex">
-                        <div className="w-fit py-2 px-4 bg-white text-black mr-2">
+                        <div className="w-fit py-2 px-4 rounded-lg bg-white text-black mr-2">
                             Nous contacter
                         </div>
-                        <div className="w-fit py-2 px-4 bg-white text-black">
+                        <div className="w-fit py-2 px-4 rounded-lg bg-white text-black">
                             Nous contacter
                         </div>
                     </div>
                 </div>
             </article>
-            <div className="relative lg:w-1/2 w-full lg:flex  h-1/2 flex-row">
+            <div className="relative lg:w-1/2 w-full lg:flex hidden h-fit flex-row">
                 <Carousel
                     infinite={true}
-                    containerProps={{
-                        style: {
-                            width: '100%',
-                            height: '100%',
-                            justifyContent: 'space-between',
-                            borderRadius: 20,
-                            overflow: 'hidden',
-                        },
-                    }}
                     itemsToShow={toShow}
                     onRequestChange={setActiveSlide}
                     easing="linear"
@@ -89,6 +78,31 @@ export default function ServicesPageServices({ params }: { params: any }) {
                     >
                         <FaArrowRight />
                     </div>
+                </div>
+            </div>
+            <div className="w-full absolute z-20 bottom-0 left-0 h-fit p-5 flex lg:hidden items-center justify-center">
+                <div
+                    onClick={() => {
+                        if (activeSlide != 0) {
+                            setActiveSlide(activeSlide - 1)
+                        } else {
+                            setActiveSlide(servicesHome[lang].length - 1)
+                        }
+                    }} className="cursor-pointer p-3 bg-white mr-4 rounded-lg hover:scale-125"
+                >
+
+                    <FaArrowLeft />
+                </div>
+                <div
+                    onClick={() => {
+                        if (activeSlide != servicesHome[lang].length - 1) {
+                            setActiveSlide(activeSlide + 1);
+                        } else {
+                            setActiveSlide(0)
+                        }
+                    }} className="cursor-pointer p-3 hover:scale-125 bg-white text-black rounded-lg"
+                >
+                    <FaArrowRight />
                 </div>
             </div>
         </div>
