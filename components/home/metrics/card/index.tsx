831e7e0ff5662d43c2fc5f19a49d36119cffe294
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-01 18:07:48 +0100

    Refactor: start counting metrics on the view

--- components/home/metrics/card/index.tsx
+++ components/home/metrics/card/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import CountUp from 'react-countup';
+import CountUp, { useCountUp } from 'react-countup';
 
 export default function HomeMetricsCard({
   number,
@@ -12,9 +12,18 @@ export default function HomeMetricsCard({
   description: string;
 }) {
   return (
-    <div className="group rounded-lg lg:w-[23%] md:w-[48%] w-full bg-white/20 mb-4 lg:mb-0 text-white py-4 px-5">
+    <div
+      id="counter"
+      className="group rounded-lg lg:w-[23%] md:w-[48%] w-full bg-white/20 mb-4 lg:mb-0 text-white py-4 px-5"
+    >
       <h2 className="md:text-7xl text-5xl font-bold mb-2 group-hover:text-blue_itm_aqua_marine">
-        <CountUp duration={3} start={0} prefix="+ " end={number} />
+        <CountUp
+          enableScrollSpy
+          duration={3}
+          start={0}
+          prefix="+ "
+          end={number}
+        />
       </h2>
       <h4 className="text-xl font-bold">{title}</h4>
       <p className="text-sm">{description}</p>
