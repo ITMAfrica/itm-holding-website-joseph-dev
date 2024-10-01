787a90472ef480c945a045d493f3c9c00f0c0a59
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-01 17:18:38 +0100

    Refactor: add of count up

--- components/home/metrics/card/index.tsx
+++ components/home/metrics/card/index.tsx
@@ -1,16 +1,20 @@
+'use client';
+
+import CountUp from 'react-countup';
+
 export default function HomeMetricsCard({
   number,
   title,
   description,
 }: {
-  number: string;
+  number: number;
   title: string;
   description: string;
 }) {
   return (
     <div className="group rounded-lg lg:w-[23%] md:w-[48%] w-full bg-white/20 mb-4 lg:mb-0 text-white py-4 px-5">
       <h2 className="md:text-7xl text-5xl font-bold mb-2 group-hover:text-blue_itm_aqua_marine">
-        {number}
+        <CountUp duration={3} start={0} prefix="+ " end={number} />
       </h2>
       <h4 className="text-xl font-bold">{title}</h4>
       <p className="text-sm">{description}</p>
