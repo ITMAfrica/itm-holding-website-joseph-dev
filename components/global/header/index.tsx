f5a2e3d5c681f0c4e3e00553b17da0a738b1f20b
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-04 10:25:17 +0100

    Refactor: add of animation to the header when scrolling

--- components/global/header/index.tsx
+++ components/global/header/index.tsx
@@ -63,7 +63,7 @@ export default function HomeHeader({ params }: propsPage) {
   return (
     <header
       className={`sticky z-50 top-0 w-full h-fit ${
-        scrollToBottom > 5
+        scrollToBottom >= 5
           ? 'py-1 transition-all duration-500 ease-linear'
           : 'md:py-4 py-3'
       }  flex justify-center bg-white shadow-headerShadow transition duration-1000`}
