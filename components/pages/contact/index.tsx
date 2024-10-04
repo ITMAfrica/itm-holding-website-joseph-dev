0ce1ffecadf98db5613465ba1365a72840cbc3e9
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-04 11:36:37 +0100

    Refactor: update of the header animations

--- components/pages/contact/index.tsx
+++ components/pages/contact/index.tsx
@@ -1,5 +1,5 @@
 import ContactPageBanner from '@/components/contact/banner';
-import ContactPageOfficesSection from '@/components/contact/offices';
+import ContactPageAboutSection from '@/components/contact/about';
 import CallToAction from '@/components/global/callToAction';
 import Faqs from '@/components/global/faqs';
 
@@ -7,7 +7,7 @@ export default function ContactPage({ params }: { params: any }) {
   return (
     <main className="w-full">
       <ContactPageBanner params={params} />
-      <ContactPageOfficesSection params={params} />
+      <ContactPageAboutSection params={params} />
       <Faqs />
       <CallToAction params={params} />
     </main>
