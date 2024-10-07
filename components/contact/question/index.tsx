cdbefafdfa6d3dfc773bd0b582ae49c27b1e772b
Amos Bafuna <amosbafuna24@gmail.com>
2024-10-07 03:19:39 +0100

    Refactor: add of images to the form section to the contact page

--- components/contact/question/index.tsx
+++ components/contact/question/index.tsx
@@ -11,39 +11,39 @@ export default function ContactPageQuestionsSection({
 }) {
   return (
     <section className="bg-white pb-10">
-      <div className="h-[350px] overflow-hidden bg-no-repeat bg-[url('../public/pages/contact/image_bg.png')]"></div>
-      <section className="w-10/12 mx-auto">
+      <div className="h-[350px] bg-no-repeat bg-[url('../public/pages/contact/image_bg.png')]"></div>
+      <section className="w-10/12 mx-auto -translate-y-72 text-white">
         <div className="w-1/2">
-          <h2>Got Questions?</h2>
-          <h4>We are happy to help</h4>
-          <p>
+          <h2 className="text-5xl font-bold">Got Questions?</h2>
+          <h4 className="text-5xl font-bold">We are happy to help</h4>
+          <p className="w-9/12 pt-5 pb-10 text-lg">
             Contact us and let's help you get started to find your dream job or
             your dream team
           </p>
-          <div className="w-fit hidden semi-lg:flex mx-auto justify-center">
+          <div className="w-fit hidden semi-lg:flex justify-start">
             <div>
               <div className="flex justify-end items-end">
-                <div className="relative md:w-[205px] w-[120px] h-fit shadow-xl rounded-md mr-3">
+                <div className="relative md:w-[170px] w-[75px] h-fit shadow-xl rounded-md mr-3">
                   <Image
                     src={image1}
                     alt="Design"
                     width={10000}
                     height={10000}
                     style={{ objectFit: 'cover' }}
-                    className="md:w-[205px] w-[120px] h-[120px] md:h-[205px] rounded-lg"
+                    className="md:w-[170px] w-[120px] h-[75px] md:h-[170px] rounded-lg"
                   />
                   <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
                 </div>
               </div>
               <div className="flex justify-end">
-                <div className="relative md:w-[155px] w-[80px] md:h-[155px] h-[80px] shadow-md rounded-lg mt-3 mr-3">
+                <div className="relative md:w-[120px] w-[80px] md:h-[120px] h-[80px] shadow-md rounded-lg mt-3 mr-3">
                   <Image
                     src={image3}
                     alt="Design"
                     width={10000}
                     height={10000}
                     style={{ objectFit: 'cover' }}
-                    className="md:w-[155px] w-[80px] md:h-[155px] h-[80px] rounded-lg"
+                    className="md:w-[120px] w-[80px] md:h-[120px] h-[80px] rounded-lg"
                   />
                   <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
                 </div>
@@ -51,28 +51,28 @@ export default function ContactPageQuestionsSection({
             </div>
             <div>
               <div className="w-1/2 md:w-1/2">
-                <div className="relative md:w-[300px] w-[225px] md:h-[300px] h-[225px] rounded-lg">
+                <div className="relative md:w-[250px] w-[225px] md:h-[250px] h-[225px] rounded-lg">
                   <Image
                     src={image2}
                     alt="Design"
                     width={10000}
                     height={10000}
                     style={{ objectFit: 'cover' }}
-                    className="md:w-[300px] w-[225px] md:h-[300px] h-[225px] rounded-lg"
+                    className="md:w-[250px] w-[225px] md:h-[250px] h-[225px] rounded-lg"
                   />
                   <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
                 </div>
               </div>
 
               <div className="w-1/2 md:w-1/2">
-                <div className="md:w-[225px] relative w-[200px] md:h-[150px] h-[150px] rounded-lg mt-3">
+                <div className="md:w-[205px] relative w-[200px] md:h-[150px] h-[150px] rounded-lg mt-3">
                   <Image
                     src={image4}
                     alt="Design"
                     width={10000}
                     height={10000}
                     style={{ objectFit: 'cover' }}
-                    className="w-[225px] md:h-[150px] h-[150px] rounded-lg"
+                    className="w-[205px] md:h-[150px] h-[150px] rounded-lg"
                   />
                   <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
                 </div>
