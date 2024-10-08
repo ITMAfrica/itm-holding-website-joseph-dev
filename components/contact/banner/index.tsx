d1bb825ef499256351259a443fc8b4817f2a5307
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-08 12:17:59 +0100

    Refactor: refactor of the contact page banner

--- components/contact/banner/index.tsx
+++ components/contact/banner/index.tsx
@@ -2,7 +2,7 @@ export default function ContactPageBanner({ params }: { params: any }) {
   return (
     <section className="w-full md:h-[60vh] h-[45vh] sm:py-10 py-5">
       <div className="flex items-center w-11/12 mx-auto text-white bg-cover bg-center bg-[url('../public/assets/bg/banner_contact.png')] h-full rounded-xl">
-        <section className="sm:w-1/2 sm:ml-20 ml-5">
+        <section className="sm:w-1/2 sm:ml-20 ml-5 mr-4 sm:mr-0">
           <h1 className="md:text-5xl text-4xl font-bold mb-4">
             Vous avez des questions?
           </h1>
