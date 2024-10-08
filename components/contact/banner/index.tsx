67501d2a491f5057ebb58ee5671f179369831f98
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-08 11:10:14 +0100

    Refactor: responsive of the banner to the contact page

--- components/contact/banner/index.tsx
+++ components/contact/banner/index.tsx
@@ -1,9 +1,11 @@
 export default function ContactPageBanner({ params }: { params: any }) {
   return (
-    <section className="w-full h-[60vh] py-10">
+    <section className="w-full md:h-[60vh] h-[45vh] sm:py-10 py-5">
       <div className="flex items-center w-11/12 mx-auto text-white bg-cover bg-center bg-[url('../public/assets/bg/banner_contact.png')] h-full rounded-xl">
-        <section className="w-1/2 ml-20">
-          <h1 className="text-5xl font-bold mb-4">Vous avez des questions?</h1>
+        <section className="sm:w-1/2 sm:ml-20 ml-5">
+          <h1 className="md:text-5xl text-4xl font-bold mb-4">
+            Vous avez des questions?
+          </h1>
           <p className="">
             Nous sommes heureux de vous répondre contactez nous et nous vous
             aiderons à trouver dès aujourd'hui l'emploi de vos rêves ou l'équipe
