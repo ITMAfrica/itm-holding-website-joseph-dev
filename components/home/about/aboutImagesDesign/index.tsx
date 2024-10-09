7787874c7122223d74e1d7e8056ea0d45df5c6ad
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-09 11:38:59 +0100

    Refactor: add of animation on the home page about section section

---
+++ components/home/about/aboutImagesDesign/index.tsx
@@ -0,0 +1,116 @@
+'use client';
+
+import Image from 'next/image';
+import image1 from '@/public/pages/home/about/image1.png';
+import image2 from '@/public/pages/home/about/image2.png';
+import image3 from '@/public/pages/home/about/image3.png';
+import image4 from '@/public/pages/home/about/image4.png';
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { a } from '@react-spring/web';
+import {
+  useSpring,
+  useSpringRef,
+  useTrail,
+  useTransition,
+} from '@react-spring/web';
+
+export default function AboutImagesDesign() {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    rootMargin: '0px 0px -480px',
+  });
+  const [viewed, setViewed] = useState<boolean>(false);
+
+  const [contentReveal, CRApi] = useTrail(
+    4,
+    {
+      from: { y: 32, opacity: 0 },
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (inView && !viewed) {
+      setViewed(true);
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    if (inView) {
+      // card animations
+      CRApi.set({ y: 32, opacity: 0 });
+      CRApi.start({
+        y: 0,
+        opacity: 1,
+        delay: 500,
+        config: { tension: 400, friction: 45 },
+      });
+    }
+  }, [viewed]);
+
+  return (
+    <div ref={ref} className="w-fit mx-auto md:w-3/5 lg:w-1/2">
+      <div className="w-fit lg:w-full hidden semi-lg:flex mx-auto flex-wrap justify-center">
+        <a.div
+          className="w-1/2 md:w-1/2 flex justify-end items-end"
+          style={contentReveal[0]}
+        >
+          <div className="relative md:w-[205px] w-[120px] h-fit shadow-xl rounded-md mr-3">
+            <Image
+              src={image1}
+              alt="Design"
+              width={10000}
+              height={10000}
+              style={{ objectFit: 'cover' }}
+              className="md:w-[205px] w-[120px] h-[120px] md:h-[205px] rounded-lg"
+            />
+            <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
+          </div>
+        </a.div>
+        <a.div className="w-1/2 md:w-1/2" style={contentReveal[1]}>
+          <div className="relative md:w-[300px] w-[225px] md:h-[300px] h-[225px] rounded-lg">
+            <Image
+              src={image2}
+              alt="Design"
+              width={10000}
+              height={10000}
+              style={{ objectFit: 'cover' }}
+              className="md:w-[300px] w-[225px] md:h-[300px] h-[225px] rounded-lg"
+            />
+            <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
+          </div>
+        </a.div>
+        <a.div
+          className="w-1/2 md:w-1/2 flex justify-end"
+          style={contentReveal[2]}
+        >
+          <div className="relative md:w-[155px] w-[80px] md:h-[155px] h-[80px] shadow-md rounded-lg mt-3 mr-3">
+            <Image
+              src={image3}
+              alt="Design"
+              width={10000}
+              height={10000}
+              style={{ objectFit: 'cover' }}
+              className="md:w-[155px] w-[80px] md:h-[155px] h-[80px] rounded-lg"
+            />
+            <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
+          </div>
+        </a.div>
+        <a.div className="w-1/2 md:w-1/2" style={contentReveal[3]}>
+          <div className="md:w-[225px] relative w-[200px] md:h-[225px] h-[200px] rounded-lg mt-3">
+            <Image
+              src={image4}
+              alt="Design"
+              width={10000}
+              height={10000}
+              style={{ objectFit: 'cover' }}
+              className="w-[225px] md:h-[225px] h-[200px] rounded-lg"
+            />
+            <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
+          </div>
+        </a.div>
+      </div>
+    </div>
+  );
+}
--- components/home/about/index.tsx
+++ components/home/about/index.tsx
@@ -1,11 +1,7 @@
 import { getDictionary } from '@/get-dictionary';
 import ButtonOulined from '@/components/global/buttons/btn_outlined';
 import SectionTitle from '@/components/global/section_title';
-import Image from 'next/image';
-import image1 from '@/public/pages/home/about/image1.png';
-import image2 from '@/public/pages/home/about/image2.png';
-import image3 from '@/public/pages/home/about/image3.png';
-import image4 from '@/public/pages/home/about/image4.png';
+import AboutImagesDesign from './aboutImagesDesign';
 
 export default function HomeAbout({ params }: { params: any }) {
   const lang: string = params.lang;
@@ -14,62 +10,7 @@ export default function HomeAbout({ params }: { params: any }) {
   return (
     <section className="md:flex justify-center w-full mx-auto lg:py-20 py-10">
       <div className="w-11/12 mx-auto flex md:flex-nowrap flex-wrap items-center  justify-center lg:justify-between">
-        <div className="w-fit mx-auto md:w-3/5 lg:w-1/2">
-          <div className="w-fit lg:w-full hidden semi-lg:flex mx-auto flex-wrap justify-center">
-            <div className="w-1/2 md:w-1/2 flex justify-end items-end">
-              <div className="relative md:w-[205px] w-[120px] h-fit shadow-xl rounded-md mr-3">
-                <Image
-                  src={image1}
-                  alt="Design"
-                  width={10000}
-                  height={10000}
-                  style={{ objectFit: 'cover' }}
-                  className="md:w-[205px] w-[120px] h-[120px] md:h-[205px] rounded-lg"
-                />
-                <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
-              </div>
-            </div>
-            <div className="w-1/2 md:w-1/2">
-              <div className="relative md:w-[300px] w-[225px] md:h-[300px] h-[225px] rounded-lg">
-                <Image
-                  src={image2}
-                  alt="Design"
-                  width={10000}
-                  height={10000}
-                  style={{ objectFit: 'cover' }}
-                  className="md:w-[300px] w-[225px] md:h-[300px] h-[225px] rounded-lg"
-                />
-                <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
-              </div>
-            </div>
-            <div className="w-1/2 md:w-1/2 flex justify-end">
-              <div className="relative md:w-[155px] w-[80px] md:h-[155px] h-[80px] shadow-md rounded-lg mt-3 mr-3">
-                <Image
-                  src={image3}
-                  alt="Design"
-                  width={10000}
-                  height={10000}
-                  style={{ objectFit: 'cover' }}
-                  className="md:w-[155px] w-[80px] md:h-[155px] h-[80px] rounded-lg"
-                />
-                <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
-              </div>
-            </div>
-            <div className="w-1/2 md:w-1/2">
-              <div className="md:w-[225px] relative w-[200px] md:h-[225px] h-[200px] rounded-lg mt-3">
-                <Image
-                  src={image4}
-                  alt="Design"
-                  width={10000}
-                  height={10000}
-                  style={{ objectFit: 'cover' }}
-                  className="w-[225px] md:h-[225px] h-[200px] rounded-lg"
-                />
-                <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-blue_itm_primary/20 rounded-lg duration-700"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <AboutImagesDesign />
         <div className="w-fit h-full md:px-5 px-3"></div>
         <div className="w-full semi-lg:w-2/5 lg:w-1/2">
           <div className="pt-10 md:pt-0 md:mr-14">
