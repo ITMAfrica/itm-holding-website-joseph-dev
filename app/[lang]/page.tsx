90fe380a80e4ef8427519845606a0dc72d7a60b5
jeereq <mingandajeereq@gmail.com>
2024-10-17 10:04:23 +0100

    feat: save change in main component in about page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,9 +1,6 @@
 import HomePage from '@/components/pages/home';
 
-export const metadata = {
-  title: 'ITM Africa | Welcome to the ITM Africa web site',
-  description: 'This is where everything start',
-};
+
 
 export default function IndexPage({ params }: { params: any }) {
   return <HomePage params={params} />;
--- components/global/cards/faq/index.tsx
+++ components/global/cards/faq/index.tsx
@@ -2,33 +2,33 @@
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 import { FaMinus } from "react-icons/fa6"
-export default function CardFaq({ about = false }) {
+export default function CardFaq({ title, description, about = false }: any) {
     const [show, setShow] = useState(false)
     function showUs() {
         setShow(!show)
     }
-    if (about) return <article className="w-full group bg-white flex flex-wrap items-center justify-between text-black p-5 pl-0 mb-5 border-b ">
+    if (about) return <article className="w-full group bg-white flex flex-wrap items-center justify-between text-black p-5 pl-0 mb-3 border-b ">
         <div onClick={showUs} className="w-full cursor-pointer h-fit flex justify-between items-start">
             <p className={`text-blue_itm_aqua_marine text-xl font-bold cursor-pointer ${!show ? "text-black" : "text-blue_itm_aqua_marine"}`}>
-                Comment puis je suivre l'etat d'une offre ?
+                {title}
             </p>
             <div className={`w-fit rounded-full p-3 py-0 group-hover:text-blue_itm_aqua_marine ${!show ? "text-black" : "text-blue_itm_aqua_marine"} text-2xl cursor-pointer`}>
                 {!show ? <FaPlus /> : <FaMinus />}
             </div>
         </div>
-        {show && <div className="w-full group-hover:text-black pt-5">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos iure itaque perspiciatis pariatur omnis praesentium ipsam, veritatis sed corrupti vel amet magni, libero asperiores dolorum laboriosam soluta, numquam tenetur placeat.
-        </div>}
+        {show && <p className="w-full group-hover:text-black pt-3">
+            {description}
+        </p>}
     </article>
-    return <article className="w-full group bg-white flex flex-wrap items-center justify-between text-black p-5 mb-5 rounded-lg">
+    return <article className="w-full group bg-white flex flex-wrap items-center justify-between text-black p-5 mb-3 rounded-lg">
         <p onClick={showUs} className={`group-hover:text-blue_itm_aqua_marine cursor-pointer ${!show ? "text-black" : "text-blue_itm_aqua_marine"}`}>
-            Comment puis je suivre l'etat d'une offre ?
+            {title}
         </p>
         <div onClick={showUs} className={`w-fit rounded-full p-3 group-hover:text-blue_itm_aqua_marine ${!show ? "text-gray_itm_bg" : "text-blue_itm_aqua_marine"} text-2xl cursor-pointer`}>
             {!show ? <FaPlus /> : <FaMinus />}
         </div>
-        {show && <div className="w-full group-hover:text-black pt-5">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos iure itaque perspiciatis pariatur omnis praesentium ipsam, veritatis sed corrupti vel amet magni, libero asperiores dolorum laboriosam soluta, numquam tenetur placeat.
-        </div>}
+        {show && <p className="w-full group-hover:text-black pt-3">
+            {description}
+        </p>}
     </article>
 }
