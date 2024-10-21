ba367a41d2a2bd37ccfced3cea589eb4f2ceba24
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-10-21 17:16:43 +0100

    Refactor: add of swiper to the home banner

--- components/home/banner/index.tsx
+++ components/home/banner/index.tsx
@@ -2,7 +2,7 @@ import HomeBannerCarousel from '@/components/home/banner/carousel';
 
 export default function HomePageBanner({ params }: { params: any }) {
   return (
-    <section className="h-[80vh] w-full">
+    <section className="h-[85vh] w-full">
       <HomeBannerCarousel params={params} toShow={1} />
     </section>
   );
