6a3d72ceda1d8102e7a1be9b4b0fe15882662b90
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-11-05 15:40:47 +0100

    Refactor: translate of the services about section

--- components/services/about/index.tsx
+++ components/services/about/index.tsx
@@ -1,32 +1,28 @@
 import SectionTitle from '@/components/global/section_title';
 import Link from 'next/link';
+import { getDictionary } from '@/get-dictionary';
 
 export default function ServicesPageAbout({ params }: any) {
+  const lang = params.lang;
+  const dictionary = getDictionary(lang);
+  const data = dictionary.globalContent.pages.services.about;
   return (
     <section className="w-full bg-white lg:py-20 py-10">
       <div className="w-11/12 lg:w-9/12 mx-auto flex lg:flex-nowrap items-start flex-wrap">
         <article className="lg:w-1/2 w-full lg:pr-20 pr-0">
-          <SectionTitle text="QUE RETENIR SUR NOUS?" />
+          <SectionTitle text={data.title} />
           <h1 className="font-bold text-4xl lg:text-5xl pt-3 text-blue_itm_primary">
-            Nous fournissons des services exclusifs pour votre entreprise
+            {data.subtitle}
           </h1>
         </article>
         <article className="lg:w-1/2 w-full lg:pt-0 pt-3">
-          <p>
-            En quête de talents ? Nous vous aidons à trouver les profils les
-            plus adaptés à vos besoins. Nous vous accompagnons dans toutes les
-            étapes de la gestion de vos ressources humaines : recrutement,
-            formation, développement des compétences, relations sociales...
-          </p>
-          <p className="pt-3">
-            Notre expertise en ressources humaines vous permet de gagner en
-            efficacité et d'améliorer les performances de vos équipes.
-          </p>
+          <p>{data.paragraph}</p>
+          <p className="pt-3">{data.paragraph2}</p>
           <Link
-            href={'/'}
+            href={data.btnContact.link}
             className="font-bold mt-5 px-5 py-3 bg-blue_itm_primary text-white block w-fit rounded-full hover:bg-blue_itm_aqua_marine"
           >
-            Contact us
+            {data.btnContact.title}
           </Link>
         </article>
       </div>
