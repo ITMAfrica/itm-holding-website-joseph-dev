7aa37c7e67462b723ba2bcd65cea5334716201ab
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-11-16 16:52:47 +0100

    Refactor: add of the service card size

--- components/services/services/cardItem/index.tsx
+++ components/services/services/cardItem/index.tsx
@@ -35,29 +35,31 @@ export default function ServiceCardText({
   const y = useParallax(scrollYProgress, 300);
 
   return (
-    <motion.section
-      ref={ref}
-      initial={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-      whileInView={{ opacity: 1, offset: ['start start', 'end end'] }}
-      viewport={{ amount: 'all', margin: '300px' }}
-      className="h-fit py-20 flex justify-center items-center snap-center border-2 border-yellow-500"
-    >
-      <div className="w-3/5">
-        <motion.h4 className="text-xl text-gray_itm">
-          Service {index + 1} #
-        </motion.h4>
-        <motion.h1 className="w-2/3 text-4xl text-blue_itm_primary font-bold py-5">
-          {name}
-        </motion.h1>
-        <p className="py-4">{paragraph}</p>
-        <p>{description}</p>
-      </div>
-      <div className="flex items-center justify-center w-2/5">
-        <div className="w-[300px] h-[400px] border border-blue_itm_primary">
-          <Image src={icon} alt={name} style={{ objectFit: 'cover' }} />
+    <div className="h-[90vh] flex items-center justify-center">
+      <motion.section
+        ref={ref}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        whileInView={{ opacity: 1, offset: ['start start', 'end end'] }}
+        viewport={{ amount: 'all' }}
+        className="h-[70vh] flex justify-center items-center snap-center border-2 border-yellow-500"
+      >
+        <div className="w-3/5">
+          <motion.h4 className="text-xl text-gray_itm">
+            Service {index + 1} #
+          </motion.h4>
+          <motion.h1 className="w-2/3 text-4xl text-blue_itm_primary font-bold py-5">
+            {name}
+          </motion.h1>
+          <p className="py-4">{paragraph}</p>
+          <p>{description}</p>
         </div>
-      </div>
-    </motion.section>
+        <div className="flex items-center justify-center w-2/5">
+          <div className="w-[300px] h-[400px] border border-blue_itm_primary">
+            <Image src={icon} alt={name} style={{ objectFit: 'cover' }} />
+          </div>
+        </div>
+      </motion.section>
+    </div>
   );
 }
