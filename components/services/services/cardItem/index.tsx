de00fca09e0ebf6f0543de17993ef077cf6e670e
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-11-16 18:02:55 +0100

    Save

--- components/services/services/cardItem/index.tsx
+++ components/services/services/cardItem/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import {
   motion,
   useScroll,
@@ -6,6 +6,7 @@ import {
   useTransform,
   useInView,
   MotionValue,
+  inView,
 } from 'framer-motion';
 import Image from 'next/image';
 
@@ -34,17 +35,44 @@ export default function ServiceCardText({
   const { scrollYProgress } = useScroll({ target: ref });
   const y = useParallax(scrollYProgress, 300);
 
+  const titleVariants = {
+    hidden: {
+      y: -100,
+      x: -50,
+      transition: {
+        duration: 0.25,
+      },
+    },
+    show: {
+      y: 0,
+      x: 0,
+      transition: {
+        duration: 0.25,
+      },
+    },
+  };
+
+  useEffect(() => {
+    if (isInView) {
+    }
+  }, [isInView]);
+
   return (
     <div className="h-[90vh] flex items-center justify-center">
       <motion.section
         ref={ref}
-        initial={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
+        initial={{ opacity: 1 }}
+        transition={{ duration: 0.5, stiffness: 'spring' }}
         whileInView={{ opacity: 1, offset: ['start start', 'end end'] }}
         viewport={{ amount: 'all' }}
         className="h-[70vh] flex justify-center items-center snap-center border-2 border-yellow-500"
       >
-        <div className="w-3/5">
+        <motion.div
+          variants={titleVariants}
+          initial="hidden"
+          animate="show"
+          className="w-3/5"
+        >
           <motion.h4 className="text-xl text-gray_itm">
             Service {index + 1} #
           </motion.h4>
@@ -53,7 +81,7 @@ export default function ServiceCardText({
           </motion.h1>
           <p className="py-4">{paragraph}</p>
           <p>{description}</p>
-        </div>
+        </motion.div>
         <div className="flex items-center justify-center w-2/5">
           <div className="w-[300px] h-[400px] border border-blue_itm_primary">
             <Image src={icon} alt={name} style={{ objectFit: 'cover' }} />
