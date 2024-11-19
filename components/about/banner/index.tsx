018694fbedface8045661bc84cb5ef666ea23799
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-11-19 10:48:02 +0100

    Refactor: add of about banner content

--- components/about/banner/index.tsx
+++ components/about/banner/index.tsx
@@ -1,18 +1,15 @@
+import { getDictionary } from '@/get-dictionary';
+
 export default function AboutPageBanner({ params }: { params: any }) {
+  const lang = params.lang;
+  const dictionary = getDictionary(lang);
+  const data = dictionary.globalContent.pages.about.banner;
   return (
     <section className="w-full sm:h-[60vh] h-[45vh] sm:py-10 py-5">
       <div className="flex items-center w-11/12 mx-auto text-white bg-cover bg-[url('../public/pages/about/banner.png')] h-full rounded-xl">
         <section className="sm:w-1/2 sm:ml-20 ml-5 mr-4 sm:mr-0">
-          <h1 className="md:text-5xl text-3xl font-bold mb-4">
-            A PROPOS DE NOUS
-          </h1>
-          <p className="">
-            Née à Lubumbashi en 2011, ITM Holding a conquis le monde en quelques
-            années. Avec plus de 10 500 agents dans 18 pays et des centaines de
-            recrutements chaque mois, cette entreprise est devenue un acteur
-            mondial des ressources humaines, prouvant que l'ambition n'a pas de
-            frontières.
-          </p>
+          <h1 className="md:text-5xl text-3xl font-bold mb-4">{data.title}</h1>
+          <p className="">{data.subtitle}</p>
         </section>
       </div>
     </section>
