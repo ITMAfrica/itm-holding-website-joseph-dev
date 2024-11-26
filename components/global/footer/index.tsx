419d1c49e4e7c151dd8135e12075d4f1470128b6
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2024-11-26 14:01:50 +0100

    Refactor: update of talent pro and kazi pro link to the footer

--- components/global/footer/index.tsx
+++ components/global/footer/index.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import logo from '@/public/logos/logo_rdc_blanc.png';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import CardCountry from '@/components/global/cards/country';
-import { CODE } from '@/helpers';
+import { CODE, KAZIPRO_HREF, TALENTPRO_HREF } from '@/helpers';
 import { useState } from 'react';
 import NavLinkFooter from './navLink';
 
@@ -77,8 +77,16 @@ export default function Footer({ params }: { params: any }) {
           <div className="mt-8 sm:mt-0">
             <h4 className="text-2xl font-bold mb-4">Nos logiciels</h4>
             <ul className="mb-2">
-              <li className="mb-3">Talent pro</li>
-              <li className="mb-3">Kazi pro</li>
+              <li className="mb-3">
+                <Link href={TALENTPRO_HREF} target="_blank">
+                  Talent pro
+                </Link>
+              </li>
+              <li className="mb-3">
+                <Link href={KAZIPRO_HREF} target="_blank">
+                  Kazi pro
+                </Link>
+              </li>
             </ul>
           </div>
           <div className="mt-8 sm:mt-0">
