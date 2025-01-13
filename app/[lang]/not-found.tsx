c0e7940bb39cbd38085140bcf7ce8b554eacd205
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2025-01-13 15:17:00 +0100

    Refactor: update of about page man section image

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -1,5 +1,14 @@
 import PageNotFound404 from '@/components/pages/page404';
+import { getDictionary } from '@/get-dictionary';
 
-export default function NotFound() {
-  return <PageNotFound404 />;
+export async function generateMetadata({ params }: any) {
+  const lang = params.lang;
+  const dictionary = getDictionary(lang);
+  const data: any = dictionary.globalContent.pages.notFound;
+
+  return data.meta;
+}
+
+export default function NotFound({ params }: { params: any }) {
+  return <PageNotFound404 params={params} />;
 }
