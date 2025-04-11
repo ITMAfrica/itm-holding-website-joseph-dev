4401c83036b89ea9b9b77579f6b5216337b4e1ab
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2025-04-11 15:23:43 +0100

    Refactor: add of senegal and Ivory cost

--- app/[lang]/[country]/[page]/page.tsx
+++ app/[lang]/[country]/[page]/page.tsx
@@ -48,15 +48,13 @@ export default function PageCountry({ params }: { params: any }) {
       return getComponent({ params });
     case 'bn':
       return getComponent({ params });
-    case 'bu':
+    case 'bi':
       return getComponent({ params });
     case 'cg':
       return getComponent({ params });
     case 'cm':
       return getComponent({ params });
     case 'de':
-      return getComponent({ params });
-    case 'bi':
       return getComponent({ params });
     case 'gb':
       return getComponent({ params });
