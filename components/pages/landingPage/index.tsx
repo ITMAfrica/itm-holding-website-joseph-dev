02c3804e9e5dc116662054d9c436c434692a72c1
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2025-05-29 18:27:34 +0100

    save 2

--- components/pages/landingPage/index.tsx
+++ components/pages/landingPage/index.tsx
@@ -1,11 +1,15 @@
 import AboutLandingPage from '@/components/landing-page/about';
 import BannerLandingPage from '@/components/landing-page/banner';
+import ProcessLandingPage from '@/components/landing-page/process';
+import StepsLandingPage from '@/components/landing-page/steps';
 
 export default function LandingPage({ params }: { params: any }) {
   return (
     <main className="w-full">
       <BannerLandingPage params={params} />
       <AboutLandingPage params={params} />
+      <ProcessLandingPage params={params} />
+      <StepsLandingPage params={params} />
     </main>
   );
 }
