db3caf4eeba044e228686458033b4e50eff5b0d1
Amos Bafuna <104500404+amos-bafuna@users.noreply.github.com>
2025-06-05 12:43:47 +0100

    Save DRC

--- components/about/banner/index.tsx
+++ components/about/banner/index.tsx
@@ -1,9 +1,12 @@
 import { getDictionary } from '@/get-dictionary';
+import { getCountryCode } from '@/helpers';
 
 export default function AboutPageBanner({ params }: { params: any }) {
   const lang = params.lang;
   const dictionary = getDictionary(lang);
-  const data = dictionary.globalContent.pages.about.banner;
+  const country: string = params.country;
+  const code: string = getCountryCode(country);
+  const data = dictionary[code].pages.about.banner;
   return (
     <section className="w-full sm:h-[60vh] h-[45vh] sm:py-10 py-5">
       <div className="flex items-center w-11/12 mx-auto text-white bg-cover bg-[url('../public/pages/about/banner.png')] h-full rounded-xl">
