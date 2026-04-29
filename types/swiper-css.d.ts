64e6761d16374f8a9fe012963cdfe0fc39c40754
josephbasix <joseph.nyandu@itmafrica.com>
2026-04-29 18:35:20 +0100

    fix minor bug

---
+++ types/swiper-css.d.ts
@@ -0,0 +1,7 @@
+declare module 'swiper/css';
+declare module 'swiper/css/bundle';
+declare module 'swiper/css/controller';
+declare module 'swiper/css/effect-fade';
+declare module 'swiper/css/mousewheel';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
